5dc5af65d27655bdca70c5a2ff565327754332d9
diez ypiens sylvain <dzs4461a@ens.fsi>
2022-10-07 09:35:25 +0200

    Fin Tp1

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -1,5 +1,52 @@
 package personnages;
 
 public class Gaulois {
+	private String nom;
+	private int force;
+	private int effetPotion=1;
 
+	public Gaulois(String nom, int force) {
+		this.nom = nom;
+		this.force = force;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+	
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "«" + texte + "» ");
+	}
+	
+	private String prendreParole() {
+		return "Le gaulois " + nom +" : ";
+	}
+	
+	public void frapper(Romain romain, int effetPotion) {
+		System.out.println(nom + "envoie un grand coup dans la machoire de " + romain.getNom());
+		romain.recevoirCoup((force/3) * effetPotion);
+		
+	}
+	
+	@Override
+	public String toString() {
+		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
+	}
+	
+	public void boirePotion(int forcePotion) {
+		this.effetPotion = forcePotion;
+		parler("Merci Druide, je sens que ma force est " + forcePotion + " fois décuplée.");
+	}
+	
+	public static void main(String[] args) {
+		Gaulois asterix = new Gaulois("Asterix",8);
+		System.out.println(asterix.getNom());
+		System.out.println(asterix);
+		
+		System.out.println(asterix.prendreParole());
+		
+		asterix.parler("Bonjour");
+		
+		asterix.boirePotion(5);
+	}
 }
--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -1,5 +1,42 @@
 package personnages;
 
 public class Romain {
-
+	private String nom;
+	private int force;
+	
+	public Romain(String nom, int force) {
+		this.nom = nom;
+		this.force = force;
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+	
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "«" + texte + "»");
+	}
+	
+	private String prendreParole() {
+		return "Le romain " + nom +" : ";
+	}
+	
+	public void recevoirCoup(int forceCoup) {
+		force -= forceCoup;
+		if (force > 0) {
+			parler("Aie");
+		}else {
+			parler("J'abandonne...");
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		Romain minus = new Romain ("Minus",6);
+		System.out.println(minus.prendreParole());
+		minus.parler("Bonjour");
+		
+		minus.recevoirCoup(5);
+		
+	}
 }
